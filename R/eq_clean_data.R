7375ae7e54bf9b3937028024cfac0d4da65c8fcd
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-11-19 21:14:40 +0000

    added roxygen header

--- R/eq_clean_data.R
+++ R/eq_clean_data.R
@@ -1,4 +1,6 @@
-#' Title
+#' Clean Raw NOAA Data
+#'
+#' The function takes a raw NOAA data frame and returns a clean data frame.
 #'
 #' @param data
 #'
@@ -6,6 +8,9 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' eq_clean_data(raw_noaa_data)
+#' }
 eq_clean_data <- function(data){
     noaa_raw_data %>%
         filter(YEAR > 1000) %>%
