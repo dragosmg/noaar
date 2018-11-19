e0789705d79319c35270267f3ab56b57228e7ce5
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-11-19 21:25:25 +0000

    added importFrom

--- R/eq_clean_data.R
+++ R/eq_clean_data.R
@@ -7,15 +7,21 @@
 #' @return
 #' @export
 #'
+#' @importFrom dplyr filter, if_else, mutate
+#' @importFrom lubridate ymd
+#' @importFrom magrittr `%>%``
+#' @importFrom rlang .data
+#' @importFrom stringr str_c
+#'
 #' @examples
 #' \dontrun{
 #' eq_clean_data(raw_noaa_data)
 #' }
 eq_clean_data <- function(data){
-    noaa_raw_data %>%
-        filter(YEAR > 1000) %>%
-        mutate(MONTH = if_else(is.na(MONTH), 1L, MONTH),
-               DAY = if_else(is.na(DAY), 1L, DAY),
-               date = str_c(YEAR, MONTH, DAY, sep = "-"),
-               date = ymd(date))
+    data %>%
+        dplyr::filter(.data$YEAR > 1000) %>%
+        dplyr::mutate(MONTH = dplyr::if_else(is.na(MONTH), 1L, MONTH),
+                      DAY = dplyr::if_else(is.na(DAY), 1L, DAY),
+                      date = stringr::str_c(YEAR, MONTH, DAY, sep = "-"),
+                      date = lubridate::ymd(date))
 }
