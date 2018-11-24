b03d08c1680c41ce4f33338c26b4e479db80009b
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-11-24 21:37:14 +0000

    test structure and tests

--- tests/testthat/test-eq-clean-data.R
+++
@@ -1,5 +0,0 @@
-context("eq_clean_data")
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
--- tests/testthat/test-eq-location-clean.R
+++
@@ -1,5 +0,0 @@
-context("eq_location_clean")
-
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
