76f5b8c0337a8f39a691a44fa7698cf3c9837f61
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-11-24 10:33:05 +0000

    added GeomTimeline

--- R/geom_timeline.R
+++ R/geom_timeline.R
@@ -30,3 +30,43 @@ geom_timeline <- function(mapping = NULL, data = NULL, stat = "identity",
         inherit.aes = inherit.aes, params = list(na.rm = na.rm, ...)
     )
 }
+
+#' @importFrom ggplot2 aes draw_key_point
+#' @importFrom grid pointsGrob linesGrob gList gpar
+#' @importFrom scales alpha
+GeomTimeline <-
+    ggplot2::ggproto(
+        "GeomTimeline", ggplot2::Geom,
+        required_aes = c("x"),
+        default_aes = ggplot2::aes(colour = "grey", size = 1.5, alpha = 0.5,
+                                   shape = 21, fill = "grey", stroke = 0.5),
+        draw_key = ggplot2::draw_key_point,
+        draw_panel = function(data, panel_scales, coord) {
+
+            if (!("y" %in% colnames(data))) {
+                data$y <- 0.15
+            }
+
+            coords <- coord$transform(data, panel_scales)
+
+            points <- grid::pointsGrob(
+                coords$x, coords$y,
+                pch = coords$shape, size = unit(coords$size / 4, "char"),
+                gp = grid::gpar(
+                    col = scales::alpha(coords$colour, coords$alpha),
+                    fill = scales::alpha(coords$colour, coords$alpha)
+                )
+            )
+            y_lines <- unique(coords$y)
+
+            lines <- grid::polylineGrob(
+                x = unit(rep(c(0, 1), each = length(y_lines)), "npc"),
+                y = unit(c(y_lines, y_lines), "npc"),
+                id = rep(seq_along(y_lines), 2),
+                gp = grid::gpar(col = "grey",
+                                lwd = .pt)
+            )
+
+            grid::gList(points, lines)
+        }
+    )
