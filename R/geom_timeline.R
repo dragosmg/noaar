d92dafac63a06e2780128a966c1ec5e5335e3fe8
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-11-24 10:18:28 +0000

    added body of function and roxygen skeleton

--- R/geom_timeline.R
+++ R/geom_timeline.R
@@ -1,1 +1,26 @@
-
+#' Title
+#'
+#' @param mapping
+#' @param data
+#' @param stat
+#' @param position
+#' @param na.rm
+#' @param show.legend
+#' @param inherit.aes
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @importFrom ggplot2 layer
+#'
+#' @examples
+geom_timeline <- function(mapping = NULL, data = NULL, stat = "identity",
+                          position = "identity", na.rm = FALSE,
+                          show.legend = NA, inherit.aes = TRUE, ...) {
+    ggplot2::layer(
+        geom = GeomTimeline, mapping = mapping, data = data, stat = stat,
+        position = position, show.legend = show.legend,
+        inherit.aes = inherit.aes, params = list(na.rm = na.rm, ...)
+    )
+}
