525333c60c475df604ba5d9a094ff1e45154fb44
Dragoș Moldovan-Grünfeld <dragos.mold@gmail.com>
2018-11-24 20:20:09 +0000

    added roxygen skeleton

---
+++ R/geom_timeline_label.R
@@ -0,0 +1,31 @@
+#' Title
+#'
+#' @param mapping
+#' @param data
+#' @param stat
+#' @param position
+#' @param show.legend
+#' @param inherit.aes
+#' @param xmin
+#' @param xmax
+#' @param n_max
+#' @param na.rm
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+geom_timeline_label <- function(mapping = NULL, data = NULL,
+                                stat = "timeline_label", position = "identity",
+                                show.legend = NA, inherit.aes = TRUE,
+                                xmin = NULL, xmax = NULL, n_max = NULL,
+                                na.rm = FALSE, ...) {
+    ggplot2::layer(
+        geom = GeomTimelineLabel, stat = stat, mapping = mapping, data = data,
+        position = position, show.legend = show.legend,
+        inherit.aes = inherit.aes,
+        params = list(xmin = xmin, xmax = xmax, n_max = n_max, na.rm = na.rm,
+            ...)
+    )
+}
